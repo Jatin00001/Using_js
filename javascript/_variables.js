20feba716faaadea0701faeba2ba4b084bf1be1c
Jatin <datajatin90@gmail.com>
2023-05-28 10:28:51 +0530

    first commit

---
+++ javascript/_variables.js
@@ -0,0 +1,9 @@
+console.log("hello")
+let a = 67; // a contain 67 nad a is identifier and we do a string literal(sinle value defined)
+
+console.log(a);
+a = "jatin";
+console.log(a)
+let b =  44 ;
+// console.log(a);
+// console.log(a-b)
